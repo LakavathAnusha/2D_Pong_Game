dde87aeda9aa982e4f2b3703858a6df15566faca
PTRL483\Anusha.Lakavath <anusha.lakavath@yesgnome.com>
2022-04-04 11:38:38 +0530

    Player Movement is done

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float playerSpeed;
+    float movementY;
+    public bool isPlayer1;
+    Rigidbody2D rb;
+    public Vector3 startPosition;
+    GameObject player1;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movementY = Input.GetAxis("Paddle1");
+        }
+        else
+        {
+            movementY = Input.GetAxis("Paddle2");
+        }
+        rb.velocity = new Vector2(rb.velocity.x, movementY * playerSpeed);
+
+    }
+
+}
